3632f6bde49282d0e307332015fdfcb15360a4f0
HaroldCone <haroldconejo123@gmail.com>
2023-08-31 20:46:24 -0500

    Subo prueba de views

--- Traviesos/facturacion/views.py
+++ Traviesos/facturacion/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def carrito_view(request):
-    context = {
-        'mensaje': 'Hola desde la vista!',
-    }
-    return render(request, 'carrito/html', context)
-
-
-
-
+    # Lógica de la vista
+    return render(request, 'carrito.html')  # Renderiza la plantilla 'carrito.html'
