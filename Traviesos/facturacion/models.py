de7674246ca3a376939abf24ea2c3e854b2122fb
HaroldCone <haroldconejo123@gmail.com>
2023-09-04 19:45:57 -0500

    .

--- Traviesos/facturacion/models.py
+++ Traviesos/facturacion/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Carrito(models.Model):
-    User = models.Foreig #uno a muchos
+class Compra(models.Model):[
+    cliente = models.ForeignKey(User, on_delete=models.CASCADE) ,
+    productos = models.ManyToManyField('Producto', through='DetalleCompra')   ,                                                      
+    fecha_compra = models.DateTimeField(auto_now_add=True),
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    
+]
+
     
