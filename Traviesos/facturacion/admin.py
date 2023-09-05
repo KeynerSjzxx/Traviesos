5ddf67713c76bc0680cdaa1d4abf0b639e395820
camilo2041 <139722810+camilo2041@users.noreply.github.com>
2023-09-04 20:48:19 -0500

    ultimas actualizaciones de los modulos

--- Traviesos/facturacion/admin.py
+++ Traviesos/facturacion/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import facturacion
+from .models import Compras
 # Register your models here.
 
-admin.site.register(facturacion)
+admin.site.register(Compras)
