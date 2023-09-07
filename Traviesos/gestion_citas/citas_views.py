8fe263ecc94925869705743dd28097bafd1ae74a
KeynerSjzxx <keynerks7@gmail.com>
2023-09-06 22:14:02 -0500

    se crean models, formulario agendar funcional

--- Traviesos/gestion_citas/citas_views.py
+++ Traviesos/gestion_citas/citas_views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import FormAgendarCita
 
-def citas (request):
-    return render(request,'citas/citas.html')
+def formulario_agendar(request):
+    if request.method == 'POST':
+        form = FormAgendarCita(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Cita guardada correctamente.')
+            form = FormAgendarCita()
+        else:
+            messages.error(request, 'Error al agendar la cita. Revise los datos.')
+            # Agrega mensajes de error específicos para cada campo con errores
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f'Error en el campo {field}: {error}')
+    else:
+        form = FormAgendarCita()
+
+    context = {'form': form}
+    return render(request, 'citas/citas.html', context)
--- Traviesos/gestion_citas/models.py
+++ Traviesos/gestion_citas/models.py
@@ -1,28 +1,67 @@
 from django.db import models
 
-# Create your models here.
-
-class Agendar_cita(models.Model):
-    tipo_cita = [
-        ('Seleccionar','Seleccionar'),
-        ('Baño','Baño'),
-        ('Peluqueada','Peluqueada'),
-        ('Baño y Peluqueada','Baño y Peluqueada'),
-    ]
-    
-    tamaño_mascota = [
-        ('Seleccionar','Seleccionar'),
-        ('Grande','Grande'),
-        ('mediano','mediano'),
-        ('pequeño','pequeño'),
-    ]
-    
-    type = models.CharField(max_length=100, choices=tipo_cita, verbose_name='Tipo cita')
-    size = models.CharField(max_length=100, choices=tamaño_mascota, verbose_name='Tamaño mascota')
-    name = models.CharField(max_length=100, verbose_name='Nombre mascosta')
-    day = models.DateField(verbose_name='Fecha de la cita')
-    hour = models.TimeField(verbose_name='Hora de la cita')
-    phone = models.CharField(max_length=15, verbose_name='Numero de contacto')
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=30, unique=True)
+    raza = models.CharField(max_length=30,default='')
+    peso = models.CharField(max_length=30, default='0')
+    edad =  models.IntegerField(verbose_name='Edad', default=0)
+    fecha_nacimiento = models.DateField(verbose_name='Fecha de nacimiento', default='2000-01-01')
     
     def __str__(self):
-        return self.name
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Mascota'
+        verbose_name_plural = 'Mascotas'
+        db_table = 'mascota'
+        ordering = ['id']
+        
+        
+class Tamaño(models.Model):
+    nombre = models.ForeignKey(Mascota, on_delete=models.CASCADE)
+    tamaño = models.CharField(
+        max_length=100,
+        choices=[
+            ('Seleccionar', 'Seleccionar'),
+            ('Grande', 'Grande'),
+            ('Mediano', 'Mediano'),
+            ('Pequeño', 'Pequeño'),
+        ],
+        verbose_name='Tamaño', default='Seleccionar'
+    )
+
+    def __str__(self):
+        return self.tamaño
+
+    class Meta:
+        verbose_name = 'tamaño'
+        verbose_name_plural = 'tamaños'
+        db_table = 'tamaño'
+        ordering = ['id']        
+        
+        
+        
+
+class AgendarCita(models.Model):
+    tipo_cita = [
+        ('Seleccionar', 'Seleccionar'),
+        ('Baño', 'Baño'),
+        ('Peluqueria', 'Peluqueria'),
+        ('Baño y Peluqueria', 'Baño y Peluqueria'),
+    ]
+    
+    Tipo_cita = models.CharField(max_length=100, choices=tipo_cita, verbose_name='Tipo cita')
+    Nombre = models.ForeignKey(Mascota, on_delete=models.CASCADE, related_name='agendarcitas_name', null=False, blank=False)
+    Fecha = models.DateField(verbose_name='Fecha de la cita')
+    Hora = models.TimeField(verbose_name='Hora de la cita')
+    Telefono = models.CharField(max_length=15, verbose_name='Número de contacto')
+    descripcion = models.TextField(max_length=200, verbose_name='Descripcion', default='')
+
+    def __str__(self):
+        return str(self.Nombre)
+    
+    class Meta:
+        verbose_name = 'Agendar cita'
+        verbose_name_plural = 'Agendar citas'
+        db_table = 'AgendarCita'
+        ordering = ['id']
