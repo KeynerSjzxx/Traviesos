c35a574b61437b49c34b2ff579d9208822e99601
HaroldCone <haroldconejo123@gmail.com>
2023-09-09 16:36:43 -0500

    Tablas modulo compras

--- Traviesos/facturacion/models.py
+++ Traviesos/facturacion/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from inventario.models import Producto
-# Create your models here.
+from inventario.models import Proveedor
 
 class Compras(models.Model):
-    
-    date = models.DateField(verbose_name='Tipo compra')
-    idProduct = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    day = models.DateField(verbose_name='Fecha de la cita')
-    hour = models.TimeField(verbose_name='Hora de la cita')
-    phone = models.CharField(max_length=15, verbose_name='Numero de contacto')
-    
-    def __str__(self):
-        return self.idProduct
+    fechaCompra = models.DateTimeField(verbose_name='Fecha de compra')
+    idProducto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantProductoComprado = models.IntegerField(verbose_name='Cantidad de productos comprados')
+    idProveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
 
-    
+    def __str__(self):
+        return f"Compra ID: {self.id}, Producto: {self.idProducto}, Cantidad: {self.cantProductoComprado}"
+
+class Proveedor(models.Model):
+    idProveedor = models.BigAutoField(primary_key=True, verbose_name='ID del proveedor')
+    Nombre_proveedor = models.CharField(max_length=30, verbose_name='Nombre del proveedor')
+    Apellido_proveedor = models.CharField(max_length=30, verbose_name='Apellido del proveedor')
+    Telefono_Proveedor = models.CharField(max_length=50, verbose_name='Teléfono del proveedor')
+
+    def __str__(self):
+        return f"{self.Nombre_proveedor} {self.Apellido_proveedor}"
