8457d238149a2e70b995156b3a183d0ee9deebf1
HaroldCone <haroldconejo123@gmail.com>
2023-09-13 18:49:16 -0500

    ya pude instalar el python

--- Traviesos/facturacion/models.py
+++ Traviesos/facturacion/models.py
@@ -1,16 +1,5 @@
 from django.db import models
-from inventario.models import Productos
-from django.contrib.auth.models import User
-# Create your models here.
-class cart(models.Model):
-    users = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE) #uno a muchos
-    productos = models.ManyToMany(Productos)
-    subtotal = models.DecimalField(default=0.0, max_digits=8, decimal_places=2)
-    total = models.DecimalField(default=0.0, max_digits=8, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return ''
 
 
     
